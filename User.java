ce19c1cf8db2ea018c86514777bcbe43890b93f7
dustinct1996 <50458389+dustinct1996@users.noreply.github.com>
2020-05-09 14:09:23 -0400

    Create User.java

---
+++ User.java
@@ -0,0 +1,11 @@
+public class User {
+	private long timeTaken;
+	private int score;
+	private String name;
+	
+	public User(long x, int y, String z) {
+		this.timeTaken = x;
+		this.score = y;
+		this.name = z;
+	}
+}
